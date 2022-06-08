833453a22dcdfa674316f809ef18e90400b35ee7
LAKSH-495 <81825279+LAKSH-495@users.noreply.github.com>
2022-06-08 14:28:09 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCNnOPeP8fKX1yHOMe72YyVgqBbF8wqeB8",
+  authDomain: "story-telling-app-7a980.firebaseapp.com",
+  databaseURL: "https://story-telling-app-7a980-default-rtdb.firebaseio.com",
+  projectId: "story-telling-app-7a980",
+  storageBucket: "story-telling-app-7a980.appspot.com",
+  messagingSenderId: "927406782161",
+  appId: "1:927406782161:web:a66134553adcaeab0e37f0"
+};
